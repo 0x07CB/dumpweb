6ae5f1b40a696a8c8372f2e51911a577a29ada62
0x07CB <83157348+0x07CB@users.noreply.github.com>
2022-03-29 21:21:27 +0200

    clean install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,27 @@
-sudo cp dumpweb.py /usr/bin/dumpweb && sudo chmod a+x /usr/bin/dumpweb && dumpweb --help
+#!/bin/bash
+function installing(){
+	echo "Install in progress..."
+	sudo cp dumpweb.py /usr/bin/dumpweb && sudo chmod a+x /usr/bin/dumpweb
+	dumpweb --help
+}
+
+if [ -e "/usr/bin/dumpweb" ]
+then
+	if [ $# -eq 1 ]
+	then
+		if [ "$1" == "-f" ]
+		then
+			installing
+		else
+			echo -e "Bad args."
+			exit 2
+		fi
+	else
+		echo -e "Is installed, For Overwrite use the flag (-f)."
+		exit 2
+	fi
+
+else
+	installing
+	exit 0
+fi
